64ec17ae9d0f2b0f5a3fb9fdf4db7995dc3797d3
joseluisgd <joseluisgd95@gmail.com>
2016-12-09 22:33:25 -0500

    done

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ admin.initializeApp({
     databaseURL: "https://schema-ulimarket.firebaseio.com"
 });
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 // Get a database reference to our posts
